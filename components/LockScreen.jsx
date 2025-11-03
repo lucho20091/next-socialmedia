497c7dd856853127d5d249a6a090a825072a5f64
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2025-11-03 12:16:17 -0500

    feat style lockscreen

--- components/LockScreen.jsx
+++ components/LockScreen.jsx
@@ -36,18 +36,18 @@ export default function LockScreen() {
         <button
           ref={buttonRef}
           onClick={() => setLocked(!locked)}
-          className={`p-3 rounded-full backdrop-blur-sm border border-white/20 transition
+          className={`p-3 rounded-full backdrop-blur-xs border border-gray-700 shadow-md shadow-gray-900 transition
             ${
               locked
-                ? "bg-black hover:bg-black/80 text-gray-400"
-                : "bg-black/40 hover:bg-black/60 text-gray-400"
+                ? "bg-black/20 hover:bg-black/40 text-gray-400"
+                : "bg-black/10 hover:bg-black/40 text-gray-400"
             }`}
         >
           {locked ? <FaUnlock size={20} /> : <FaLock size={20} />}
         </button>
       </div>
       {locked && (
-        <div className="absolute left-5 top-5 bg-black/50 backdrop-blur-xl px-4 py-2 text-white shadow-xl border border-gray-700 rounded-md shadow-gray-800">
+        <div className="absolute left-5 top-5 bg-black/10 backdrop-blur-xs px-4 py-2 text-gray-300 shadow-md border border-gray-700 rounded-md shadow-gray-900">
           <span>Screen Locked</span>
         </div>
       )}
